8c419166dc28cb334d72628c3e6c48a1398299bf
Maximilian Deubel <maximilian.deubel@gmail.com>
2018-07-18 12:19:58 +0200

    finally got it working

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,45 @@
-#define F_CPU 1200000L
-#include <avr/delay.h>
 #include <avr/interrupt.h>
 #include <avr/power.h>
 #include <avr/sleep.h>
+#include <util/delay.h>
 
-volatile uint8_t greeting = 1;
-volatile uint8_t starting_up = 1;
 volatile uint8_t go_to_sleep = 0;
 
-// 5v copter
-ISR(INT0_vect)
-{
-    if (PINB & (1 << PB1))
-    {
-        greeting = 1;
-        go_to_sleep = 1;
-    }
+__attribute__((always_inline)) static void greeting(void) {
+  PORTB |= (1 << PB0);
+  for (uint8_t i = 0; i < 5; ++i) {
+    _delay_ms(100);
+    PORTB ^= (1 << PB0);
+  }
+  PORTB &= ~(1 << PB0);
 }
 
-int main(void)
-{
-    cli();
-    if (starting_up)
-    {
-        starting_up = 0;
-        power_all_disable();
-        DDRB = (1 << PB0);
-        MCUCR |= (0 << PUD) | (1 << ISC00);
-        GIMSK |= (1 << INT0);
-        go_to_sleep = 1;
-    }
-    if (greeting)
-    {
-        greeting = 0;
-        PORTB |= (1 << PB0);
-        for (uint8_t i = 0; i < 5; ++i)
-        {
-            _delay_ms(100);
-            PORTB ^= (1 << PB0);
-        }
-        PORTB &= ~(1 << PB0);
-    }
-    if (go_to_sleep || (PINB & (1 << PB2)) || (PINB & (1 << PB1)))
-    {
-        go_to_sleep = 0;
-        PORTB &= ~(1 << PORTB0);
-        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-        sei();
-        sleep_mode();
+// 5v from copter 10k Pulldown -> PB1
+// Button 10k Pulldown -> PB2
+// Beeper out -> PB0
+ISR(PCINT0_vect) {
+  if (PINB & (1 << PB1)) {
+    greeting();
+  }
+}
+
+int main(void) {
+  power_all_disable();
+  DDRB = (1 << PB0);
+  GIMSK |= (1 << PCIE);
+  PCMSK |= (1 << PCINT1);
+
+  greeting();
+
+  while (1) {
+    if ((PINB & (1 << PB2)) || (PINB & (1 << PB1))) {
+      go_to_sleep = 0;
+      PORTB &= ~(1 << PORTB0);
+      set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+      sei();
+      sleep_mode();
     }
     PORTB ^= (1 << PB0);
     _delay_ms(500);
+  }
 }
