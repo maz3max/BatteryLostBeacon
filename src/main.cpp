54b6507a1f02c4bf31367b6985ed2f5927df5f30
Maximilian Deubel <maximilian.deubel@gmail.com>
2018-07-20 04:24:44 +0200

    more responsive button handling

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <util/delay.h>
 
 volatile uint8_t go_to_sleep = 0;
+volatile uint8_t last_button_state = 0;
 
 __attribute__((always_inline)) static void greeting(void) {
   PORTB |= (1 << PB0);
@@ -11,14 +12,17 @@ __attribute__((always_inline)) static void greeting(void) {
     _delay_ms(100);
     PORTB ^= (1 << PB0);
   }
-  PORTB &= ~(1 << PB0);
 }
 
 // 5v from copter 10k Pulldown -> PB1
 // Button 10k Pulldown -> PB2
 // Beeper out -> PB0
 ISR(PCINT0_vect) {
-  if (PINB & (1 << PB1)) {
+  uint8_t button_state = PINB & (1 << PB2);
+  if (button_state != last_button_state) {
+    last_button_state = button_state;
+    go_to_sleep = 1;
+  } else if (PINB & (1 << PB1)) {
     greeting();
   }
 }
@@ -27,12 +31,12 @@ int main(void) {
   power_all_disable();
   DDRB = (1 << PB0);
   GIMSK |= (1 << PCIE);
-  PCMSK |= (1 << PCINT1);
-
+  PCMSK |= (1 << PCINT1) | (1 << PCINT2);
+  last_button_state = PINB & (1 << PB2);
   greeting();
 
   while (1) {
-    if ((PINB & (1 << PB2)) || (PINB & (1 << PB1))) {
+    if (go_to_sleep == 1 || (PINB & (1 << PB2)) || (PINB & (1 << PB1))) {
       go_to_sleep = 0;
       PORTB &= ~(1 << PORTB0);
       set_sleep_mode(SLEEP_MODE_PWR_DOWN);
